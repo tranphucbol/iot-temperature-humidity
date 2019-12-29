6d6312b7843cba73274c722f5376e912e34816b6
anhhuy980413 <1653030@student.hcmus.edu.vn>
2019-12-29 18:39:30 +0700

    DS tree

--- mqtt/index.js
+++ mqtt/index.js
@@ -4,8 +4,12 @@ const { places } = config;
 const db = require("../database");
 const moment = require("moment");
 const logger = require("../logger");
+const DecisionTree = require('decision-tree'); 
 
 const data = {};
+let dataTrain =[];
+let decisionTree;
+let count = 0;
 
 const client = mqtt.connect(config.mqtt.url, {
     username: config.mqtt.username,
@@ -37,17 +41,27 @@ const client = mqtt.connect(config.mqtt.url, {
     const topics = Object.keys(data);
     topics.forEach(topic => client.subscribe(topic));
     logger.info(`Subscribe ${JSON.stringify(topics)}`);
+    trainDecisionTree();
 })();
 
 client.on("message", async (topic, message) => {
     // message is Buffer
-
+    if(count == 50){
+        trainDecisionTree();
+        count = 0;
+    }
     if (data[topic] !== undefined) {
         const { temperature, humidity } = JSON.parse(message.toString());
         const time = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
+        let predictLightStatus = decisionTree.predict({
+            temperature: temperature,
+            humidity: humidity,
+            placeId: data[topic].id,
+            noon: Math.floor(curHour/6)
+        });
         await db.query(
-            "INSERT INTO temp_humi_log (placeId, temperature, humidity, logTime) VALUES (?, ?, ?, ?)",
-            [data[topic].id, temperature, humidity, time]
+            "INSERT INTO temp_humi_log (placeId, temperature, humidity, logTime, lightStatus) VALUES (?, ?, ?, ?, ?)",
+            [data[topic].id, temperature, humidity, time, predictLightStatus]
         );
         data[topic] = {
             ...data[topic],
@@ -58,7 +72,27 @@ client.on("message", async (topic, message) => {
     }
 });
 
+async function trainDecisionTree(){
+    let sql = `
+        SELECT thl.placeId, thl.temperature, thl.humidity, thl.logtime, thl.lightStatus
+        FROM temp_humi_log thl 
+        WHERE auto=0`;
+    let result = await db.query(sql);
+    let preData = result[0];
+    preData.forEach(e =>{
+        curHour = e.logtime.getHours();
+        e.noon = Math.floor(curHour/6);
+        delete e.logtime;
+    })
+    var features = ["placeId", "temperature", "humidity", "noon"];
+    var class_name = "lightStatus";
+    decisionTree = new DecisionTree(preData, class_name, features);
+}
+
+
+
 module.exports = {
     data,
-    clientMQTT: client
+    clientMQTT: client,
+    count
 };
